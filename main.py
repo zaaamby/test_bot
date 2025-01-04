3264d68d9a0c53a5be94f36ce6cca3d51a4eb6ea
Alexandr Matveev <zzaammbbyy@gmail.com>
2025-01-04 12:00:27 +0300

    Изменение функций - реагирование на тип сообщения (фото, гс, текст)

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет\nМеня зовут Эхо-бот!\nПопробуй что-нибудь мне написать!')
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне любое сообщение, а я отправлю тебе его в ответ!')
 
@@ -28,22 +30,29 @@ async def process_sent_voice(message: Message):
     await message.answer(text="Вы прислали голосовое сообщение")'''
 
 # Этот хэндлер будет срабатывать на отправку боту фото
+@dp.message(F.voice)
+async def send_voice_echo(message: Message):
+    #print(message)
+    await message.answer('Вы прислали голосовое сообщение!')
+
+@dp.message(F.photo)
 async def send_photo_echo(message: Message):
-    print(message)
+    #print(message)
     await message.answer('Вы прислали фото!')
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
     await message.answer('Вы прислали текстовое сообщение!')
 
 
 
 # Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands='start'))
+'''dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
 dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_echo)
+dp.message.register(send_echo)'''
 
 if __name__ == '__main__':
     dp.run_polling(bot)
