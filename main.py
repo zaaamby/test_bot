862e827a33305d84f6b5c32ee7ab240748d60e17
Alexandr Matveev <zzaammbbyy@gmail.com>
2025-01-04 19:41:23 +0300

    Добавление распознавание видео

--- main.py
+++ main.py
@@ -29,21 +29,30 @@ async def process_sent_voice(message: Message):
     #отправляем сообщение в чат откуда пришло сообщение
     await message.answer(text="Вы прислали голосовое сообщение")'''
 
-# Этот хэндлер будет срабатывать на отправку боту фото
+# Этот хэндлер будет срабатывать на отправку боту гс
 @dp.message(F.voice)
 async def send_voice_echo(message: Message):
     #print(message)
+    print(message)
     await message.answer('Вы прислали голосовое сообщение!')
 
+# Этот хэндлер будет срабатывать на отправку боту фото
 @dp.message(F.photo)
 async def send_photo_echo(message: Message):
-    #print(message)
+    print(message)
     await message.answer('Вы прислали фото!')
+
+# Этот хэндлер будет срабатывать на отправку боту видео
+@dp.message(F.video)
+async def send_video_echo(message: Message):
+    print(message)
+    await message.answer('Вы прислали видео!')
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
+    print(message)
     await message.answer('Вы прислали текстовое сообщение!')
 
 
